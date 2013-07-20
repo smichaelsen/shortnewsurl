7960baebd7cece443c1172e5bc4e4354f9000fe3
Sebastian Michaelsen <sebastian.michaelsen@t3seo.de>
2013-07-20 10:48:13 +0200

    Make use of the new model class also in single view

--- Classes/Controller/NewsController.php
+++ Classes/Controller/NewsController.php
@@ -5,11 +5,11 @@ class Tx_Shortnewsurl_Controller_NewsController extends Tx_News_Controller_NewsC
 	/**
 	 * Single view of a news record
 	 *
-	 * @param Tx_News_Domain_Model_News $news news item
+	 * @param Tx_Kfinewsapi_Domain_Model_News $news news item
 	 * @param integer $currentPage current page for optional pagination
 	 * @return void
 	 */
-	public function detailAction(Tx_News_Domain_Model_News $news = NULL, $currentPage = 1) {
+	public function detailAction(Tx_Kfinewsapi_Domain_Model_News $news = NULL, $currentPage = 1) {
 
 		if (is_null($news) && !$this->settings['singleNews']) {
 			$this->forward('list');
